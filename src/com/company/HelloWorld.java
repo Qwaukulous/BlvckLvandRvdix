007e14cc6d19a340af947d144b8b4f134f8ebc32
qwaukulous <qwauknation@yahoo.com>
2019-04-17 13:27:37 -0400

    First program Task. Team ~BlvckLvnd Rvdix 66.6

---
+++ src/com/company/HelloWorld.java
@@ -0,0 +1,19 @@
+package com.company;
+import java.util.Scanner;
+public class HelloWorld {
+    public static void main(String[] args) {
+
+
+        System.out.println("Hello, World!");
+        System.out.println("Today is April 18,2019. I am alive!");
+        System.out.println("My name is Qwauk");
+
+
+    Scanner keyboard = new Scanner(System.in);
+    System.out.println("Enter your first initial");
+    String firstInitial = keyboard.nextLine();
+    System.out.println(firstInitial);
+
+
+    }
+}
